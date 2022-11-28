f5a9497da5a7ad2dc56c8d1df4410ec4f5265bcf
Filipp G <filfilet@mail.ru>
2022-11-28 22:22:27 +0300

    work1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num_a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_b = int.Parse(Console.ReadLine());
+
+if (num_a < num_b)
+{
+    Console.WriteLine("Первое число " + num_a + " меньше чем второе " + num_b);
+}
+else
+{
+Console.WriteLine("Второе число " + num_b + " меньше чем первое " + num_a);
+}
